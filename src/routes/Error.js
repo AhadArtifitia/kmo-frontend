bafb3719b2bc1040c276bb346d1d9dc550db217a
Abdul Ahad <hadi@Haadis-MacBook-Air.local>
2023-04-17 14:26:10 +0530

    error page

--- src/routes/Error.js
+++ src/routes/Error.js
@@ -1,10 +1,17 @@
 import React from 'react'
+import Footer from '../components/Footer'
+import Nav from '../components/Nav'
+import './Error.css'
 
 const Error = () => {
   return (
-    <div>
-      <h1>404! This page could not be found!</h1>
+    <>
+    <Nav />
+    <div className='error'>
+      <h1>404 | This page could not be found!</h1>
     </div>
+    <Footer />
+    </>
   )
 }
 
